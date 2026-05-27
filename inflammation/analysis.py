564d1d2c2f6158679674eac6e3c1d0fe81c22c32
Luca-Pasqualin <l.pasqualin@tudelft.nl>
2026-05-27 11:24:55 +0200

    add CSV class

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -8,13 +8,28 @@ import argparse
 from inflammation import models, views
 
 
-def load_inflammation_data(data_dir):
+class CSVDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
 
-    data_file_paths = glob.glob(os.path.join(data_dir, "inflammation*.csv"))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-    return list(data)
+    def load_inflammation_data(self):
+
+        data_file_paths = glob.glob(os.path.join(self.data_dir, "inflammation*.csv"))
+        if len(data_file_paths) == 0:
+            raise ValueError(
+                f"No inflammation data CSV files found in path {self.data_dir}"
+            )
+        data = map(models.load_csv, data_file_paths)
+        return list(data)
+
+
+# def load_inflammation_data(data_dir):
+
+#     data_file_paths = glob.glob(os.path.join(data_dir, "inflammation*.csv"))
+#     if len(data_file_paths) == 0:
+#         raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+#     data = map(models.load_csv, data_file_paths)
+#     return list(data)
 
 
 def analyse_data(data_dir):
@@ -24,7 +39,9 @@ def analyse_data(data_dir):
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
 
-    data = load_inflammation_data(data_dir)
+    data_source = CSVDataSource(data_dir=data_dir)
+    data = data_source.load_inflammation_data()
+    # data = load_inflammation_data(data_dir)
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
@@ -32,6 +49,7 @@ def analyse_data(data_dir):
 
     graph_data = {"standard deviation by day": daily_standard_deviation}
     views.visualize(graph_data)
+    return daily_standard_deviation
 
 
 if __name__ == "__main__":
